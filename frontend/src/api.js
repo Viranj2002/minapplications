c11c0d3f1c63b63df2d7c84c0dc783aa4f113046
Viranj Masdeakr <viranjrmasdekar@gmail.com>
2025-12-07 21:59:38 +0530

    Fix render.yaml property and update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const API_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL
+    ? `https://${import.meta.env.VITE_API_URL}`
+    : "http://localhost:8000";
 
 export const fetchApplications = async () => {
     const response = await fetch(`${API_URL}/applications`);
